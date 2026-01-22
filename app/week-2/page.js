bbdf23bd30ceff30189f4f4b31038aaa33f2f3e3
Santiago pabon <santiago.pabon72@gmail.com>
2026-01-22 16:53:21 -0700

    Added displayStudentInfo component and added student info to week-2 page

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import DisplayStudentInfo from "./studentInfo"
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <DisplayStudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/studentInfo.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function DisplayStudentInfo(){
+  return(
+    <div>
+    <h3>Name: Santiago Pabon</h3>
+    <Link href="https://github.com/Santi447">GitHub: https://github.com/Santi447</Link>
+    </div>
+  );
+}
