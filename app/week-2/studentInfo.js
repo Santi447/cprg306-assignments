09c478287d5b49b4190327af231796e4bf698e7f
Santiago pabon <santiago.pabon72@gmail.com>
2026-01-23 15:34:31 -0700

    Added link to repo and not my profile and deleted the deploy code in inital page.js file

--- app/week-2/studentInfo.js
+++ app/week-2/studentInfo.js
@@ -3,7 +3,7 @@ export default function DisplayStudentInfo(){
   return(
     <div>
     <h3>Name: Santiago Pabon</h3>
-    <Link href="https://github.com/Santi447">GitHub: https://github.com/Santi447</Link>
+    <Link href="https://github.com/Santi447/cprg306-assignments">GitHub: https://github.com/Santi447/cprg306-assignments</Link>
     </div>
   );
 }
