749b23804f7afd3aa62c9dd7712987e949e598a1
santiago pabon <santiago.pabon72@gmail.com>
2026-01-26 20:19:51 -0700

    added styling to h1

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import GroceryItemList from "./GroceryItemList";
 export default function Page(){
     return(
         <main class="p-4 max-w-xl mx-auto">
-            <h1>Grocery Item</h1>
+            <h1 class="text-3xl font-bold mb-3">Grocery Item</h1>
             <GroceryItemList />
         </main>
     );
