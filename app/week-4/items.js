134ed65e76dd444b6ec13bff9792c1d2b07f3ced
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-05 15:18:31 -0700

    Created the Items component for week 4

---
+++ app/week-4/items.js
@@ -0,0 +1,11 @@
+
+
+export default function Item({name,quantity,category}){
+  return(
+  <li>
+      <p>Name: {name}</p>
+      <p>Quantity: {quantity}</p>
+      <p>Category: {category}</p>
+  </li>
+  );
+}
