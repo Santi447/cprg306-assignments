5d25fc00c1000e76b62e61b419a9c18f5fdfa5a0
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-06 23:44:05 -0700

    revised style revised style

--- app/week-4/items.js
+++ app/week-4/items.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Item({name,quantity,category,imageUrl}){
   return(
-  <li className="flex flex-col items-center bg-white p-6 border border-default rounded-xl shadow-xs md:flex-row md:max-w-xl md:flex-row md:max-w-xl  ">
+  <li className="flex flex-col items-center bg-white p-6 border border-default rounded-xl shadow-xs md:flex-row md:max-w-xl">
       <Image src={imageUrl} alt={name} width={80} height={80} className='object-cover w-full rounded-xl h-64 md:h-auto md:w-48 mb-4 md:mb-0'/>
       <div className="flex flex-col justify-between md:p-4 leading-normal">
       <p className="mb-2 text-2xl font-bold tracking-tight text-heading text-black">Name: {name}</p>
