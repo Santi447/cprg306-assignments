290a17ab526334fd3984549f702468c02e12c4ae
santiago <santiago.pabon72@gmail.com>
2026-02-10 18:45:52 -0700

    created the week-5 folder and added page.js and newItem.js as well as added week 5 section in my root page.js file

--- app/page.js
+++ app/page.js
@@ -30,7 +30,14 @@ export default function Home() {
         </Link>
         </p>
       </li>      
-
+      <li className="my-2 bg-stone-300 dark:bg-stone-600 p-4 rounded-xl">
+        <p>
+        <Link href="week-5"
+              className="hover:text-blue-600 dark:hover:text-blue-300  duration-300 ease-linear">
+        Go to Week 5
+        </Link>
+        </p>
+      </li> 
     </ul>
   </main>
   );
