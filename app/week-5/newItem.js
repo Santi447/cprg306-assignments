d8df3239c83427a9b59629ba239485a62906fc25
santiago <santiago.pabon72@gmail.com>
2026-02-13 23:03:33 -0700

    removed duplicate css

--- app/week-5/newItem.js
+++ app/week-5/newItem.js
@@ -26,7 +26,7 @@ export default function NewItem() {
               value={name}
               onChange={(element)=> {setName(element.target.value)}}
               required={false}
-              className="w-full p-2 rounded-md bg-neutral-900 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+              className="p-2 rounded-md bg-neutral-900 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
               />
             </label>
             <div className="flex flex-row  gap-10"> 
