355cc6a8bfdac2f913cc0f7a969f55a55c863979
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-26 14:51:03 -0700

    Added main and header with h1 inside for header

---
+++ app/week-7/page.js
@@ -0,0 +1,11 @@
+export default function Page(){
+  return(
+    <main>
+      <header>
+        <h1 className="mx-auto max-w-2xl p-4 text-center text-3xl font-bold">
+          Week 7
+        </h1>
+      </header>
+    </main>
+  )
+}
