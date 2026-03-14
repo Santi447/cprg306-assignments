97876da5f79bb422627990b56f3f08aac38915eb
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-14 10:49:46 -0600

    working on creating a login and signin form components

---
+++ app/components/LoginForm.js
@@ -0,0 +1,25 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+export default function LoginForm(){
+  return(
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+    </Card>
+    // <form>
+    //   <label htmlFor="username">
+    //     Username
+    //   </label>
+    //   <input type="text"></input>
+    // </form>
+  );
+}
