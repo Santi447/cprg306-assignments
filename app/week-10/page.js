e4f5027c41c42ff7ddaa1436cd81edc168de0f18
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-27 21:14:01 -0600

    Update page.js

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -1,16 +1,15 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useUserAuth } from "../../app/context/AuthContext";
 
 export default function Page() {
   const { user } = useUserAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) {
+    
       router.replace("/week-10/login");
-    }
-  }, [router, user]);
+    
+  }, [router]);
   return null;
 }
