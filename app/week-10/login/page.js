d1531cc1e5ca77178f2c865f6ce5f8d6cec7b3ff
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-27 20:47:30 -0600

    removed the if user blocks add more problems then solves tbh

--- app/week-10/login/page.js
+++ app/week-10/login/page.js
@@ -1,25 +1,9 @@
-"use client";
+
 import LoginForm from "../../components/LoginForm";
-import { useUserAuth } from "../../context/AuthContext";
-import Link from "next/link";
 export default function Page() {
-  const { user } = useUserAuth();
-  if (!user) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-slate-950">
         <LoginForm url="/week-10/shopping-list" signupUrl="/week-10/signup" />
       </div>
     );
-  }
-  if (user) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-slate-950">
-        <p className="text-white">You are already logged in.</p>
-        <p className="text-white">please log out to access this page.</p>
-        <Link className="text-blue-800" href="/week-10/shopping-list">
-          Go back to shopping list to log out
-        </Link>
-      </div>
-    );
-  }
 }
--- app/week-10/signup/page.js
+++ app/week-10/signup/page.js
@@ -1,23 +1,10 @@
+"use client";
 import SignUpForm from "@/app/components/SignUpForm";
-import { useUserAuth } from "../../context/AuthContext";
+
 export default function Page() {
-  const { user } = useUserAuth();
-  if (!user) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-slate-950">
         <SignUpForm url="/week-10/signup" loginUrl="/week-10/login" />
       </div>
     );
-  }
-  if (user) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-slate-950">
-        <h1 className="text-3xl text-white">You are already logged in.</h1>
-        <p>please log out to access this page.</p>
-        <Link className="text-white" href="/week-10/shopping-list">
-          Go back to shopping list to log out
-        </Link>
-      </div>
-    );
-  }
 }
