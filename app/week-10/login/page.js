5863b3082c84ea8dcb277e3587f944157a971438
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-27 20:40:07 -0600

    added more protection as to only show the login or signup form if logged out. need to ask next time how to avoid the flashing

--- app/week-10/login/page.js
+++ app/week-10/login/page.js
@@ -1,10 +1,25 @@
+"use client";
 import LoginForm from "../../components/LoginForm";
-export default function Page(){
-    return(
-
-    <div className="flex items-center justify-center min-h-screen bg-slate-950">
-    <LoginForm url="/week-10/shopping-list" signupUrl="/week-10/signup"/>
-    </div>
+import { useUserAuth } from "../../context/AuthContext";
+import Link from "next/link";
+export default function Page() {
+  const { user } = useUserAuth();
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-slate-950">
+        <LoginForm url="/week-10/shopping-list" signupUrl="/week-10/signup" />
+      </div>
     );
-
+  }
+  if (user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-slate-950">
+        <p className="text-white">You are already logged in.</p>
+        <p className="text-white">please log out to access this page.</p>
+        <Link className="text-blue-800" href="/week-10/shopping-list">
+          Go back to shopping list to log out
+        </Link>
+      </div>
+    );
+  }
 }
--- app/week-10/page.js
+++ app/week-10/page.js
@@ -1,11 +1,16 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useUserAuth } from "../../context/AuthContext";
 
 export default function Page() {
+  const { user } = useUserAuth();
   const router = useRouter();
+
   useEffect(() => {
-    router.replace("/week-10/login");
-  }, [router]);
+    if (!user) {
+      router.replace("/week-10/login");
+    }
+  }, [router, user]);
   return null;
 }
